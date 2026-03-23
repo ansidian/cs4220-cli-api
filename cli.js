9728f2af6e5b48b13f5e65962bdf7e71e704b62c
ansidian <andysu1551@gmail.com>
2026-03-23 15:54:12 -0700

    feat: entry point - yargs setup, command definitions, --help, route to app.js

---
+++ cli.js
@@ -0,0 +1,49 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+import { search } from "./app.js";
+
+yargs(hideBin(process.argv))
+  .command(
+    // example: `node cli.js search -t track 'best i ever had'`
+    "search <keyword>",
+    "search Spotify by keyword",
+    (yargs) => {
+      yargs
+        .positional("keyword", {
+          describe: "search query",
+          type: "string",
+        })
+        .option("type", {
+          describe: "type of search",
+          alias: "t",
+          type: "string",
+          choices: ["artist", "track", "album"],
+          default: "artist",
+        });
+    },
+    (args) => {
+      search(args.keyword, args.type);
+    },
+  )
+  .command(
+    // example: `node cli.js history keywords`
+    "history <action>",
+    "look at previous search history",
+    (yargs) => {
+      yargs.positional("action", {
+        describe: "type of history to view",
+        type: "string",
+      });
+    },
+    (args) => {
+      if (args.action === "keywords") {
+        // TODO: call history from history.js
+        // pending history.js completion
+      } else {
+        console.log(`Invalid action: ${args.action}`);
+      }
+    },
+  )
+  // `node cli.js --help`
+  .help().argv;
